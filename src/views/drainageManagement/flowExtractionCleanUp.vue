2224d59b7b4155b25808fa23242cc8acf806c895
gina00 <584525974@qq.com>
2022-02-23 14:10:17 +0800

    路由配置，增加引流管理模块

---
+++ src/views/drainageManagement/flowExtractionCleanUp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page">
+
+    <el-row>
+      <el-col :span="16" :gutter="20">
+        <el-tabs type="border-card">
+          <el-tab-pane label="流量提取">流量提取</el-tab-pane>
+          <el-tab-pane label="流量解析">流量解析</el-tab-pane>
+          <el-tab-pane label="流量清理">流量清理</el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col :span="8">
+        <div>日志面板</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.page{
+  padding: 20px;
+}
+</style>
